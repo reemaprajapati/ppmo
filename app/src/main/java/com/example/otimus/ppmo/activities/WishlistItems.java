e6263ea94c5fe643a212fb2ae49d36b7f2484a75
ichchha <sassyichchha@gmail.com>
2017-08-08 14:50:53 +0545

    layout design

---
+++ app/src/main/java/com/example/otimus/ppmo/activities/WishlistItems.java
@@ -0,0 +1,28 @@
+package com.example.otimus.ppmo.activities;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.otimus.ppmo.R;
+
+public class WishlistItems extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wishlist_items);
+
+        fab=(FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(WishlistItems.this,NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/otimus/ppmo/model/Category.java
@@ -0,0 +1,32 @@
+package com.example.otimus.ppmo.model;
+
+
+
+public class Category {
+    private int img;
+    private String title;
+
+    public Category() {
+    }
+
+    public Category(int img, String title) {
+        this.img = img;
+        this.title = title;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
